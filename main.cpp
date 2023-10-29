18916bdff07774c4ce17d0a85eea62b4b5b48f3e
Ajivajijaajjerat <dankcoolguy123@gmail.com>
2023-10-28 20:59:21 -0700

    Add basic function for normalizing data to values between 0 and 1.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,25 @@
 #include <vector>
 #include "Classes/Node.h"
 #include "Classes/NeuralNet.h"
+#include "algorithm"
 
-class NeuralNet;
+std::vector<double> normalizeData(std::vector<double>& inputData) {
+	auto max = std::max_element(inputData.begin(), inputData.end());
+	std::cout << "Max: " << *max << std::endl;
+	for (auto &value: inputData) {
+		value = value / *max;
+		std::cout << "Normalized input: " << value << std::endl;
+	}
+}
 
 
 
 int main() {
+	std::vector<double> inputs {312,532,856};
+	normalizeData(inputs);
 	std::vector<int> netTopology {3, 3, 3};
 	NeuralNet neuralNet(netTopology);
-	neuralNet.FeedInputs();
+	neuralNet.FeedInputs(inputs);
 	neuralNet.FeedForward();
 	return 0;
 }
