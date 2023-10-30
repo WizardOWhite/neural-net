6009d9f03464d17278ca380839e19e23091cd098
Ajivajijaajjerat <dankcoolguy123@gmail.com>
2023-10-30 02:55:44 -0700

    Add functions and variables necessary for backpropagation.

--- Classes/Node.cpp
+++ Classes/Node.cpp
@@ -6,8 +6,8 @@ Node::Node(int numInputs, int layerNumber, int nodeNumber) : _input(0), _output(
 		auto randomWeight = static_cast<double>(std::rand()) / RAND_MAX;
 		inputWeights.emplace_back(Weight{randomWeight, 0});
 		std::cout << "Random weight: " << randomWeight
-		<< " || Layer: " << layerNumber
-		<< " || Node: " << nodeNumber << std::endl;
+		<< " || Layer: " << layerNumber + 1
+		<< " || Node: " << nodeNumber + 1 << std::endl;
 	}
 }
 
@@ -22,8 +22,45 @@ void Node::FeedForward(const std::vector<Node>& prevLayer) {
 	}
 	//Activation function
 	_output = tanh(_input);
+	std::cout << _nodePosition.layerNumber << ":" << _nodePosition.nodeNumber << ": " << _input << " -> " << _output << std::endl;
 }
 
 double Node::GetOutput() const {
 	return _output;
+}
+
+void Node::CalculateHiddenGradients(std::vector<Node> &nextLayer) {
+	double derivativesOfWeights = SumOfDOW(nextLayer);
+	_gradient = derivativesOfWeights * ActivationFunctionDerivative(_output);
+}
+
+void Node::UpdateWeights(std::vector<Node> &prevLayer) {
+	for(int i = 0; i < prevLayer.size(); i++) {
+		Node &node = prevLayer[i];
+		double oldDeltaWeight = inputWeights[i].deltaWeight;
+
+		double newDeltaWeight = learningRate * node._output * _gradient + alpha * oldDeltaWeight;
+	}
+}
+
+void Node::CalculateOutputGradients(double expectedOutput) {
+	double delta = expectedOutput - _output;
+	_gradient = delta * ActivationFunctionDerivative(_output);
+}
+
+double Node::ActivationFunctionDerivative(double output) {
+	return 1.0 - (output * output);
+}
+
+double Node::SumOfDOW(std::vector<Node> &nextLayer) {
+	double sum = 0;
+
+	for(int i = 0; i < nextLayer.size(); i++) {
+		sum += nextLayer[i].GetInputWeight(_nodePosition.nodeNumber) * nextLayer[i]._gradient;
+	}
+	return sum;
+}
+
+double Node::GetInputWeight(int position) {
+	return inputWeights[position].weight;
 }
--- Classes/Node.h
+++ Classes/Node.h
@@ -18,13 +18,22 @@ class Node {
 private:
 	double _input;
 	double _output;
+	double _gradient;
 	std::vector<Weight> inputWeights;
+	double learningRate = 0.15;
+	double alpha = 0.5;
 public:
 	Position _nodePosition;
 	Node(int numInputs, int layerNumber, int nodeNumber);
 	void SetInput(double input);
 	void FeedForward(const std::vector<Node>& prevLayer);
 	double GetOutput() const;
+	void CalculateOutputGradients(double expectedOutput);
+	void CalculateHiddenGradients(std::vector<Node> &nextLayer);
+	void UpdateWeights(std::vector<Node> &prevLayer);
+	double ActivationFunctionDerivative(double output);
+	double SumOfDOW(std::vector<Node> &vector);
+	double GetInputWeight(int position);
 };
 
 #endif //BASICNEURALNET_NODE_H
